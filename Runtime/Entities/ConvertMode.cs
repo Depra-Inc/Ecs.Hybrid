624fcc9843b7b7a66fbef4ae0803f4749461351b
godzzz <g0dzZz1lla@yandex.ru>
2023-10-03 23:22:52 +0400

    Inspector names for ConvertMode

--- Runtime/Entities/ConvertMode.cs
+++ Runtime/Entities/ConvertMode.cs
@@ -1,12 +1,19 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
 // © 2023 Nikolay Melnikov <n.melnikov@depra.org>
 
+using UnityEngine;
+
 namespace Depra.Ecs.Baking.Runtime.Entities
 {
 	internal enum ConvertMode
 	{
+		[InspectorName("Convert and inject into world")]
 		CONVERT_AND_INJECT,
+
+		[InspectorName("Convert and destroy")]
 		CONVERT_AND_DESTROY,
+
+		[InspectorName("Convert and save")]
 		CONVERT_AND_SAVE
 	}
 }
