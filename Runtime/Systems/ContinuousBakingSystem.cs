d2552581f15a8d62b6f78c7a6b9194cdacb490ae
godzzz <g0dzZz1lla@yandex.ru>
2024-01-09 00:46:22 +0400

    [v0.1.4] Fixes for Il2Cpp attributes

--- Runtime/Systems/ContinuousBakingSystem.cs
+++ Runtime/Systems/ContinuousBakingSystem.cs
@@ -1,5 +1,5 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
-// © 2023 Nikolay Melnikov <n.melnikov@depra.org>
+// © 2023-2024 Nikolay Melnikov <n.melnikov@depra.org>
 
 using Depra.Ecs.Components;
 using Depra.Ecs.Entities;
@@ -8,12 +8,11 @@ using Depra.Ecs.QoL.Entities;
 using Depra.Ecs.QoL.Worlds;
 using Depra.Ecs.Systems;
 using Depra.Ecs.Worlds;
+using Unity.IL2CPP.CompilerServices;
 
 namespace Depra.Ecs.Hybrid.Systems
 {
 #if ENABLE_IL2CPP
-	using Unity.IL2CPP.CompilerServices;
-
 	[Il2CppSetOption(Option.NullChecks, false)]
 	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
 #endif
