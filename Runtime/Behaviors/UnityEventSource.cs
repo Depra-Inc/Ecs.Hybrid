146d337d150e20c456a90c9ca1cfe4d4aba8af48
n.melnikov <g0dzZz1lla@yandex.ru>
2025-02-26 01:53:19 +0400

    [v0.3.1] Authoring event source
    * [v0.3.1-pre.1] Add UnityEventSource from ecs.unity
    * [v0.3.1-pre.2] Protected world for event source
    * [v0.3.1] Increase version

---
+++ Runtime/Behaviors/UnityEventSource.cs
@@ -0,0 +1,33 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2023-2025 Depra <n.melnikov@depra.org>
+
+using System.Runtime.CompilerServices;
+using Depra.Ecs.Hybrid;
+using Depra.Ecs.QoL;
+using UnityEngine;
+#if ENABLE_IL2CPP
+using Unity.IL2CPP.CompilerServices;
+#endif
+
+namespace Depra.Ecs.Unity
+{
+#if ENABLE_IL2CPP
+	[Il2CppSetOption(Option.NullChecks, false)]
+	[Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+#endif
+	[DefaultExecutionOrder(100)]
+	public abstract class UnityEventSource : AuthoringBehaviour
+	{
+		[SerializeField] private string _senderName;
+
+		public UnityEventSender Sender => new(_senderName, gameObject);
+		protected World World { get; private set; }
+
+		protected override void Bake(World world) => World = world;
+
+		protected virtual bool Validate() => World != null;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected virtual PackedEntityWithWorld CreateEntity() => World.PackEntityWithWorld(World.CreateEntity());
+	}
+}
