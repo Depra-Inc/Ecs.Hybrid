a20755b58eafbd9d0a24695e848f6533779d8701
n.melnikov <n.melnikov@depra.org>
2026-05-02 19:14:58 +0400

    [v0.3.8] Update serialize reference extensions dependency
    * +
    
    * [v0.3.8-pre.1] bump version
    
    * Bump version to 0.3.8 and update dependencies

--- Runtime/Components/ComponentSerializeReference.cs
+++ Runtime/Components/ComponentSerializeReference.cs
@@ -1,5 +1,5 @@
 ﻿// SPDX-License-Identifier: Apache-2.0
-// © 2023-2024 Nikolay Melnikov <n.melnikov@depra.org>
+// © 2023-2026 Depra <n.melnikov@depra.org>
 
 using System;
 using System.Collections.Generic;
@@ -14,16 +14,23 @@ namespace Depra.Ecs.Hybrid
 
 		public ComponentSerializeReference(string nameSubstring) => _nameSubstring = nameSubstring;
 
-		public override IEnumerable<Type> GetTypes(Type referenceType) =>
-			from assembly in AppDomain.CurrentDomain.GetAssemblies()
-			from extractedType in assembly.GetTypes()
-			where extractedType.IsPublic &&
-			      extractedType.IsValueType &&
-			      !extractedType.IsGenericType &&
-			      extractedType.FullName!.Contains(_nameSubstring) &&
-			      !IsDefined(extractedType, HideSerializeReferenceAttribute.TYPE)
-			      // TODO: Uncomment this line after obsolete serialization is removed.
-			      // && extractedType.GetCustomAttribute<SerializableAttribute>() != null
-			select extractedType;
+		public override IEnumerable<Type> GetDerivedTypes(Type referenceType)
+		{
+#if UNITY_EDITOR
+			return from extractedType in UnityEditor.TypeCache.GetTypesDerivedFrom(referenceType)
+				where extractedType.IsPublic &&
+				      extractedType.IsValueType &&
+				      !extractedType.IsGenericType &&
+				      extractedType.FullName!.Contains(_nameSubstring) &&
+				      !IsDefined(extractedType, HideSerializeReferenceAttribute.TYPE)
+				// TODO: Uncomment this line after obsolete serialization is removed.
+				// && extractedType.GetCustomAttribute<SerializableAttribute>() != null
+				select extractedType;
+#else
+			return Array.Empty<Type>();
+#endif
+		}
+
+		public override IEnumerable<Type> GetGenericTypes(Type referenceType) => Array.Empty<Type>();
 	}
 }
